f6cef0dec673adca54ce46e7df58c22a938f7da5
Houwaida Hidri <houwaidahidri@gmail.com>
2025-07-30 14:51:53 +0100

    Add files via upload

---
+++ Code/catkin_ws/build/CTestTestfile.cmake
@@ -0,0 +1,15 @@
+# CMake generated Testfile for 
+# Source directory: /home/hidri/mémoire/catkin_ws/src
+# Build directory: /home/hidri/mémoire/catkin_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("minilab_demo_hardware_remote")
+subdirs("minilab_demo_simulation")
+subdirs("minilab_model")
+subdirs("minilab_simulation")
+subdirs("scripts")
+subdirs("minilab_navigation")
+subdirs("minilab_teleop")
+subdirs("safe_teleop")
